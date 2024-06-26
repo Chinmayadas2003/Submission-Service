36f7232451b65c010bc2e8846c3efe6fd1119272
chinmaya <chinmayadas2003@rediffmail.com>
2024-06-27 03:11:02 +0530

    added connectDB call

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const apiPlugin=require('./routes/api/apiRoutes');
 const serverConfig=require('./config/serverConfig')
 
 const app=require('./app');
+const connectToDB = require('./config/db.config');
 
 fastify.register(app);//automatically takes fastify as a parameter
 //fastify.register(apiPlugin)
@@ -15,11 +16,12 @@ fastify.get('/', function handler (request, reply) {
 })
 
 // Run the server!
-fastify.listen({ port: serverConfig.PORT }, (err) => {
+fastify.listen({ port: serverConfig.PORT }, async(err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
+  await connectToDB();
   console.log("server started")
 })
 //fastify is building its own ecosystem.
